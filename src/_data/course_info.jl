b142e37c6044627f717765acec7a61916e2650bb
Dongdong Kong <kongdd@users.noreply.github.com>
2023-11-15 10:56:49 +0800

    add climate_science from MIT

--- src/_data/course_info.jl
+++ src/_data/course_info.jl
@@ -1,13 +1,13 @@
 Dict(
-    "course_name" => "Name of your course",
-    "course_subtitle" => "a short catchy phrase about your course",
-    "code" => "code of your course",
-    "semester" => "Fall 20XX",
-    "authors" => [
-        "First author name" => "first author homepage link",
-        "Second author name" => "second author homepage link"
-    ],
-    "institution" => "Your university (or similar) name",
-    "institution_logo" => "name of your institution logo file, e.g. `logo.svg`. Place this under `assets`",
-    "repo" => "link to the repository of the source code"
+  "course_name" => "2023CUG水文气象学",
+  "course_subtitle" => "",
+  "code" => "",
+  "semester" => "Fall 2023",
+  "authors" => [
+    "Dongdong Kong" => "https://github.com/kongdd",
+    # "Second author name" => "second author homepage link"
+  ],
+  "institution" => "中国地质大学（武汉）",
+  "institution_logo" => "name of your institution logo file, e.g. `logo.svg`. Place this under `assets`",
+  "repo" => "https://github.com/CUG-hydro/class2023_CUG_Programming"
 )
