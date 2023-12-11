e0f7d55bc5abbc41b54c1c086bfcde68581fc5df
Dongdong Kong <kongdd@users.noreply.github.com>
2023-12-11 14:27:53 +0800

    fix bug

--- src/_data/sidebar.jl
+++ src/_data/sidebar.jl
@@ -1,7 +1,7 @@
 [
-    "welcome" => "Welcome",
-    "module1" => "Module 1: Website setup",
-    "module2" => "Module 2: Adding course material",
-    "module3" => "Module 3: Publishing your website",
-    "chapter02" => "ch02_大气的基本特征",
+  "welcome" => "Welcome",
+  "module1" => "Module 1: Website setup",
+  "module2" => "Module 2: Adding course material",
+  "module3" => "Module 3: Publishing your website",
+  "ch02" => "ch02_大气的基本特征",
 ]
